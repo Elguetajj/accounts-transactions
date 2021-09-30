b0d3f588792229f2dcda2fe5cad82df5d94c4f9d
elguetajj <elguetaj@ufm.edu>
2021-09-30 16:27:39 -0600

    adds less faulty script

--- accounts.py
+++ accounts.py
@@ -2,35 +2,44 @@ import time
 import json
 import random
 import argparse
+import uuid
 
 import requests 
 
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
-rpc_user = '123'
-rpc_password = '123'
 
-account_pool_endpoint = "http://localhost:80/wallet"
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18443"%(rpc_user, rpc_password))
+with open('config.json','r') as conf:
+    configs = json.loads(conf.read())
+
+
+print(configs)
+rpc_user = configs['rpc_user']
+rpc_password = configs['rpc_user']
+
+account_pool_endpoint = configs['account_pool_endpoint']
+
 
 parser = argparse.ArgumentParser(description='Personal information')
 parser.add_argument('--speed', dest='speed', type=int, help='how many accounts per minute')
 
-node_name = 'nodoTest'
-name = "name"
+node_name = configs['node_name']
+names = configs['names']
 
 # rpc_connection.getnewaddress(node_name)
 if __name__ == "__main__":
     args = parser.parse_args()
     while(True):
-        count = 1
-        new_address = rpc_connection.getnewaddress(f'{node_name}.{name}{count}')
+        rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18443"%(rpc_user, rpc_password))
+        name = random.choice(names)
+        uid = uuid.uuid4()
+        new_address = rpc_connection.getnewaddress(f'{node_name}.{name}{uid}')
         data = json.dumps({
-            "name": f'{node_name}.{name}{count}',
+            "name": f'{node_name}.{name}{uid}',
             "address": new_address
         })
         print(data)
-        r = requests.post(account_pool_endpoint,data)
+        r = requests.post(f'{account_pool_endpoint}/account',data)
         print(r.text)
         time.sleep(60/args.speed)
 
---
+++ transactions.py
@@ -0,0 +1,127 @@
+from decimal import Decimal
+import decimal
+import time 
+import json
+import random
+import argparse
+
+import requests 
+
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+with open('config.json','r') as conf:
+    configs = json.loads(conf.read())
+
+rpc_user = configs['rpc_user']
+rpc_password = configs['rpc_user']
+
+account_pool_endpoint = configs['account_pool_endpoint']
+
+
+parser = argparse.ArgumentParser(description='Personal information')
+parser.add_argument('--speed', dest='speed', type=int, help='how many accounts per minute')
+
+node_name = configs['node_name']
+names = configs['names']
+
+# rpc_connection.getnewaddress(node_name)
+if __name__ == "__main__":
+    args = parser.parse_args()
+    while(True):
+        rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18443"%(rpc_user, rpc_password))
+    
+        listunspent = []
+        while(not listunspent):
+            r = requests.get(f'{account_pool_endpoint}/account/{node_name}')
+            print("sender:",sender:=r.json()["address"])
+            listunspent = rpc_connection.listunspent(0,9999,[f"{sender}"])
+
+            print("unspent:",listunspent)
+        
+        unspent = random.choice(listunspent)
+        while(unspent["amount"]<0.1):
+            unspent = random.choice(listunspent)
+
+            
+
+        balance =  unspent['amount']
+        print("-------------")
+        print("unspent:",unspent)
+        print(unspent['spendable'])
+        if unspent['spendable']:
+            can_spend = 1
+            txid  = unspent['txid']
+            vout = 0
+            amount_to_spend = Decimal(random.uniform(0,float(balance)*0.5))
+            fee = Decimal(0.00001111)
+            change = balance-(amount_to_spend+fee)
+
+        
+    
+        
+        # unspent = random.choice(listunspent)
+        # balance =  unspent['amount']
+        # print("balance:",balance)
+        # print("spendable?:",unspent['spendable'])
+        # if unspent['spendable']:
+        #     txid  = unspent['txid']
+        #     vout = 0
+        #     amount_to_spend = Decimal(random.uniform(0,float(balance)*0.5))
+        #     fee = balance*Decimal(0.05)
+        #     change = balance-amount_to_spend-fee
+        #     break
+
+            
+        reciever = requests.get(f'{account_pool_endpoint}/account/{node_name}').json()["address"]
+
+        print("amount_to_spend:",amount_to_spend)
+        print("change:",change)
+        print("fee:",fee)
+
+        rawtransaction =rpc_connection.createrawtransaction(
+            [{"txid":txid,"vout":0}],
+            {reciever:amount_to_spend,sender:change}
+        )
+
+        print("rawtransaction:",rawtransaction)
+
+        privkey = rpc_connection.dumpprivkey(
+            sender
+        )
+        print("key:",privkey)
+
+        signed_transaction = rpc_connection.signrawtransactionwithkey(
+            rawtransaction,
+            [privkey]
+        )
+        print("signed transaction:",signed_transaction)
+
+        hex = signed_transaction["hex"]
+        print("hex:",hex)
+        sent_rawtransaction = rpc_connection.sendrawtransaction(
+            hex
+        )
+        print("sent_transaction:",sent_rawtransaction)
+
+        time.sleep(60/args.speed)
+
+    
+
+
+
+# ./bitcoin-cli createrawtransaction "[{\"txid\":\"2f7aa4499f94371c942d326da5127c8cb15ea97b82c31453338de7eb15844d25\",\"vout\":0}]" "{\"2NBVRHmHS3ReJ7sTQxxAmVHyak9BwWUnxzA\":1.0,\"2N91bwsns4Hd5rLLMVMUVhqut17oq3L96nN\":45.52992940}"
+
+# 0200000001254d8415ebe78d335314c3827ba95eb18c7c12a56d322d941c37949f49a47a2f0000000000ffffffff0200e1f5050000000017a914c8218580adab134ef396f41436d5543af89ce8e887ac28610f0100000017a914acee84e3dc1719d398b82287ccd0db658561cbec8700000000
+
+# ./bitcoin-cli signrawtransactionwithkey "0200000001254d8415ebe78d335314c3827ba95eb18c7c12a56d322d941c37949f49a47a2f0000000000ffffffff0200e1f5050000000017a914c8218580adab134ef396f41436d5543af89ce8e887ac28610f0100000017a914acee84e3dc1719d398b82287ccd0db658561cbec8700000000" "[\"cUCcoPJx2vaKL7CAAPbissa4bLuWivzyCC3JWzGTWhqVYkavc7Yc\"]"
+
+# {
+#   "hex": "02000000000101254d8415ebe78d335314c3827ba95eb18c7c12a56d322d941c37949f49a47a2f000000001716001449ca9ac7497e741737f03f8f160b929de23f0924ffffffff0200e1f5050000000017a914c8218580adab134ef396f41436d5543af89ce8e887ac28610f0100000017a914acee84e3dc1719d398b82287ccd0db658561cbec8702473044022068665fa3259cd8e1fa9067ece0a05699546fa65a484d0370da7589311727092402204a5b36dbfe9d9c43df3fc09f971dd1cef28902b08b7354ea860e4f20df9e7965012103bd20c009a1d867482a4d1691d35ee7b7a228046f2c442a42a0b655336237ea0d00000000",
+#   "complete": true
+# }
+
+# ./bitcoin-cli sendrawtransaction 02000000000101254d8415ebe78d335314c3827ba95eb18c7c12a56d322d941c37949f49a47a2f000000001716001449ca9ac7497e741737f03f8f160b929de23f0924ffffffff0200e1f5050000000017a914c8218580adab134ef396f41436d5543af89ce8e887ac28610f0100000017a914acee84e3dc1719d398b82287ccd0db658561cbec8702473044022068665fa3259cd8e1fa9067ece0a05699546fa65a484d0370da7589311727092402204a5b36dbfe9d9c43df3fc09f971dd1cef28902b08b7354ea860e4f20df9e7965012103bd20c009a1d867482a4d1691d35ee7b7a228046f2c442a42a0b655336237ea0d00000000
+
+# 636620655b14c862b76aeef301029d1d147c559ab1152a137a7c78d7ffb47683
+
+# ./bitcoin-cli listunspent 0 99999 "[\"2NBVRHmHS3ReJ7sTQxxAmVHyak9BwWUnxzA\"]"
